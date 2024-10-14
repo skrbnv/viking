49963e83f36d310421af665feff88de307338e07
Sergey <sergey.skrebnev@gmail.com>
2024-10-14 17:42:35 +0300

    no-shadow

--- src/app/page.js
+++ src/app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="absolute z-10 w-screen h-48 bg-gradient-to-b from-black to-transparent"></div>
       <div className="absolute bottom-0 z-10 w-screen h-96 bg-gradient-to-t from-black to-transparent"></div>
       <div className="absolute z-20 bottom-32 w-full max-w-3xl left-1/2 transform -translate-x-1/2 text-center pl-3 pr-3">
-        <h1 className="text-5xl font-sans font-black text-black drop-shadow-[0_0_2px_white]">COME TO POWER WITH VIKING DATA CENTERS</h1>
+        <h1 className="text-5xl font-sans font-black text-white">COME TO POWER WITH VIKING DATACENTERS</h1>
         <p className="font-sans text-lg italic text-white">Empowering the decentralized future of crypto mining and AI with resilient energy infrastructure.</p>
         <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center pt-5">
           <a
